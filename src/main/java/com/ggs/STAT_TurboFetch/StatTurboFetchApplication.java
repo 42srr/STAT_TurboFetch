cf4373451ebd74d4006daaaf64840e52481cc8c1
조재영 <whwodud0303@naver.com>
2024-12-30 17:09:56 +0900

    first init

---
+++ src/main/java/com/ggs/STAT_TurboFetch/StatTurboFetchApplication.java
@@ -0,0 +1,13 @@
+package com.ggs.STAT_TurboFetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatTurboFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatTurboFetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggs/STAT_TurboFetch/StatTurboFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggs.STAT_TurboFetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatTurboFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
